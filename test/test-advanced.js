213a922308b49d81962227a7c1c983550d6afa79
Benjamin Winterberg <winterbe@googlemail.com>
2015-01-06 07:35:46 +0100

    Tests

--- test/test-advanced.js
+++ test/test-advanced.js
@@ -47,3 +47,68 @@ QUnit.test("filter - map - toArray (correct invocation)", function (assert) {
     assert.equal(numFilter, 4);
     assert.equal(numMap, 2);
 });
+
+var createParents = function (numParents, numChildren) {
+    return Stream
+        .range(0, numParents)
+        .map(function (num) {
+            return {
+                parentId: num,
+                type: 'parent',
+                children: []
+            };
+        })
+        .peek(function (parent) {
+            parent.children = Stream
+                .range(0, numChildren)
+                .map(function (num) {
+                    return {
+                        childId: num,
+                        type: 'child',
+                        parent: parent
+                    };
+                })
+                .toArray();
+        })
+        .toArray();
+};
+
+QUnit.test("parent / children 1", function (assert) {
+    var parents = createParents(5, 3);
+
+    assert.equal(parents.length, 5);
+
+    for (var i = 0; i < parents.length; i++) {
+        var parent = parents[i];
+        assert.equal(parent.parentId, i);
+        assert.equal(parent.type, 'parent');
+        assert.equal(parent.children.length, 3);
+        for (var j = 0; j < parent.children.length; j++) {
+            var child = parent.children[j];
+            assert.equal(child.childId, j);
+            assert.equal(child.type, 'child');
+            assert.equal(child.parent, parent);
+        }
+    }
+});
+
+QUnit.test("parent / children 2", function (assert) {
+    var parents = createParents(5, 3);
+
+    var children = Stream(parents)
+        .filter(function (p) {
+            return p.parentId > 2;
+        })
+        .flatMap(function (p) {
+            return p.children;
+        })
+        .toArray();
+
+    assert.equal(children.length, 6);
+    assert.equal(children[0].childId, 0);
+    assert.equal(children[1].childId, 1);
+    assert.equal(children[2].childId, 2);
+    assert.equal(children[3].childId, 0);
+    assert.equal(children[4].childId, 1);
+    assert.equal(children[5].childId, 2);
+});
