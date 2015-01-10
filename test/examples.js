6636049ec4247c63b91af710288603edae49f4bf
Benjamin Winterberg <winterbe@googlemail.com>
2015-01-10 08:43:42 +0100

    Clean up test code

--- test/examples.js
+++ test/examples.js
@@ -1,29 +1,4 @@
 QUnit.test("filter - map - toArray", function (assert) {
-    var data = [1, 2, 3, 4];
-
-    var result =
-        Stream(data)
-            .filter(function (num) {
-                return num % 2 === 1;
-            })
-            .map(function (num) {
-                return "obj" + num;
-            })
-            .toArray();
-
-    assert.equal(result.length, 2);
-    assert.equal(result[0], 'obj1');
-    assert.equal(result[1], 'obj3');
-
-    // assert original data is untouched
-    assert.equal(data.length, 4);
-    assert.equal(data[0], 1);
-    assert.equal(data[1], 2);
-    assert.equal(data[2], 3);
-    assert.equal(data[3], 4);
-});
-
-QUnit.test("filter - map - toArray (correct invocation)", function (assert) {
     var numFilter = 0;
     var numMap = 0;
 
@@ -46,6 +21,13 @@ QUnit.test("filter - map - toArray (correct invocation)", function (assert) {
     assert.equal(result[1], 'obj3');
     assert.equal(numFilter, 4);
     assert.equal(numMap, 2);
+
+    // assert original data is untouched
+    assert.equal(data.length, 4);
+    assert.equal(data[0], 1);
+    assert.equal(data[1], 2);
+    assert.equal(data[2], 3);
+    assert.equal(data[3], 4);
 });
 
 var createParents = function (numParents, numChildren) {
--- test/operations.js
+++ test/operations.js
@@ -216,56 +216,6 @@ QUnit.test("flatMap", function (assert) {
     assert.equal(data[2], 3);
 });
 
-QUnit.test("filter map", function (assert) {
-    var data = [1, 2, 3, 4];
-
-    var result =
-        Stream(data)
-            .filter(function (num) {
-                return num % 2 === 1;
-            })
-            .map(function (num) {
-                return "obj" + num;
-            })
-            .toArray();
-
-    assert.equal(result.length, 2);
-    assert.equal(result[0], 'obj1');
-    assert.equal(result[1], 'obj3');
-
-    // assert original data is untouched
-    assert.equal(data.length, 4);
-    assert.equal(data[0], 1);
-    assert.equal(data[1], 2);
-    assert.equal(data[2], 3);
-    assert.equal(data[3], 4);
-});
-
-QUnit.test("filter map (correct invocation)", function (assert) {
-    var numFilter = 0;
-    var numMap = 0;
-
-    var data = [1, 2, 3, 4];
-
-    var result =
-        Stream(data)
-            .filter(function (num) {
-                numFilter++;
-                return num % 2 === 1;
-            })
-            .map(function (num) {
-                numMap++;
-                return "obj" + num;
-            })
-            .toArray();
-
-    assert.equal(result.length, 2);
-    assert.equal(result[0], 'obj1');
-    assert.equal(result[1], 'obj3');
-    assert.equal(numFilter, 4);
-    assert.equal(numMap, 2);
-});
-
 QUnit.test("findFirst", function (assert) {
     var result =
         Stream([1, 2, 3, 4])
