45f28b6a2eb6b2ebeb354dfedf19dad6f2400bf2
Benjamin Winterberg <winterbe@gmail.com>
2015-01-22 09:14:01 +0100

    Set version 1.3.0 for next release

--- test/test-misc.js
+++ test/test-misc.js
@@ -26,7 +26,7 @@ QUnit.test("toString", function (assert) {
 });
 
 QUnit.test("version", function (assert) {
-    assert.equal(Stream.VERSION, "1.2.0");
+    assert.equal(Stream.VERSION, "1.3.0");
 });
 
 QUnit.test("noConflict", function (assert) {
