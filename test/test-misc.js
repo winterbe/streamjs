8ae851d289a0b4fad88ac1ea7692884ffa6824ff
Benjamin Winterberg <winterbe@gmail.com>
2015-06-26 08:48:21 +0200

    Version 1.6.0

--- test/test-misc.js
+++ test/test-misc.js
@@ -26,7 +26,7 @@ QUnit.test("toString", function (assert) {
 });
 
 QUnit.test("version", function (assert) {
-    assert.equal(Stream.VERSION, "1.5.0");
+    assert.equal(Stream.VERSION, "1.6.0");
 });
 
 QUnit.test("noConflict", function (assert) {
