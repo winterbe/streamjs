fa21de7a1c0cfb34ea1b625a3fa9bf5cfe419125
Benjamin Winterberg <winterbe@googlemail.com>
2015-09-20 14:02:21 +0200

    Migrate to ESLint #46

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
             files: ['test/index.html']
         },
         eslint: {
-            target: ['stream.js']
+            target: ['stream.js', 'test']
         }
     });
 
